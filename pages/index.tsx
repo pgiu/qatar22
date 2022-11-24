3ce42c2e5c8f04d214bfb95dca25d5bc6ed3490f
Pablo Giudice <pablo.giudice@emilabs.ai>
2022-11-24 09:27:20 -0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -136,7 +136,7 @@ export async function getStaticProps() {
     },
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
-    // - At most once every 10 seconds
+    // - At most once every 60 seconds
     revalidate: 60, // In seconds
   }
 }
